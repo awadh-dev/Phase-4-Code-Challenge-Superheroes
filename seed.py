161a840f23ddb202c4875190a8a56df4029396a5
awadh-dev <awadhabed@gmail.com>
2025-06-18 10:13:17 +0300

    updated Models.py

---
+++ seed.py
@@ -0,0 +1,46 @@
+from app import app
+from models import db, Hero, Power, HeroPower
+
+with app.app_context():
+    
+    HeroPower.query.delete()
+    Power.query.delete()
+    Hero.query.delete()
+
+
+    heroes = [
+        Hero(name="Kamala Khan", super_name="Ms. Marvel"),
+        Hero(name="Doreen Green", super_name="Squirrel Girl"),
+        Hero(name="Gwen Stacy", super_name="Spider-Gwen"),
+        Hero(name="Janet Van Dyne", super_name="The Wasp"),
+        Hero(name="Wanda Maximoff", super_name="Scarlet Witch"),
+        Hero(name="Carol Danvers", super_name="Captain Marvel"),
+        Hero(name="Jean Grey", super_name="Dark Phoenix"),
+        Hero(name="Ororo Munroe", super_name="Storm"),
+        Hero(name="Kitty Pryde", super_name="Shadowcat"),
+        Hero(name="Elektra Natchios", super_name="Elektra")
+    ]
+    db.session.add_all(heroes)
+
+    
+    powers = [
+        Power(name="super strength", description="gives the wielder super-human strengths"),
+        Power(name="flight", description="gives the wielder the ability to fly through the skies at supersonic speed"),
+        Power(name="super human senses", description="allows the wielder to use her senses at a super-human level"),
+        Power(name="elasticity", description="can stretch the human body to extreme lengths")
+    ]
+    db.session.add_all(powers)
+    db.session.commit()
+
+    
+    hero_powers = [
+        HeroPower(strength="Strong", hero_id=1, power_id=2),
+        HeroPower(strength="Average", hero_id=3, power_id=1),
+        HeroPower(strength="Weak", hero_id=2, power_id=4),
+        HeroPower(strength="Strong", hero_id=5, power_id=1),
+        HeroPower(strength="Average", hero_id=6, power_id=2),
+    ]
+    db.session.add_all(hero_powers)
+    db.session.commit()
+
+    print(" Seed data inserted successfully.")
